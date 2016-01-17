5773e0dc5cc98d0e761cb5d7fb9117a8182a6d65
Linghan Cheung <2661301@qq.com>
2016-01-17 13:36:57 +0800

    add alert action

---
+++ public/javascripts/editor.js
@@ -0,0 +1,38 @@
+"use strict";
+
+$(function ()
+{
+    var ue = UE.getEditor('container', 
+    {
+        serverUrl: "/admin/edit/control", 
+        initialFrameHeight: 500
+    });
+    console.log(ue.getOpt("serverUrl")); 
+    ue.on("serverConfigLoaded", function(){console.log(ue.getOpt("imageActionName"));});
+    
+    $("#publishBtn").bind("click", function()
+        {
+            console.log("click Publish button " + ue.getContent());
+            $(".alert.alert-info").hide();
+            $(".alert.alert-info").slideDown("slow");
+            $.ajax("/admin/edit/store", 
+            {
+                method: "post",
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+                data: {content: ue.getContent()}
+            }).done(function(data)
+            {
+                $(".alert.alert-info").hide();
+                $(".alert.alert-success").slideDown("slow");
+            }).fail(function(xhr, status)
+            {
+                $(".alert.alert-info").hide();
+                $(".alert.alert-danger").slideDown("slow");
+            }).always(function()
+            {
+                console.log("click Publish button always");
+            });
+        }
+    );
+}
+);
