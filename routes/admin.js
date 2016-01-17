ae765e1f6b1609c788cf005021d921e6e304d56e
Linghan Cheung <2661301@qq.com>
2016-01-17 22:37:09 +0800

    initialize the ueditor configuration

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,22 @@
 var express = require("express");
 var adminRouter = express.Router();
 var ueditorController = require("../react/ueditor_controller");
+var editor = require("../react/editor");
 
 adminRouter.get("/edit", function (req, res, next)
 {
     res.render("edit", {});
 });
 
-adminRouter.all("/edit/control", ueditorController());
+adminRouter.all("/edit/control", ueditorController(
+    {
+        rootDir: "/Users/lhcheung1991/VScodeProjects/blog_site/",
+        imagesDir: "public/images/",
+        filesDir: "public/files/",
+        configJsonFile: "/Users/lhcheung1991/VScodeProjects/blog_site/react/config.json"
+    }
+));
 
-
-
+adminRouter.post("/edit/store", editor);
 
 module.exports = adminRouter;
