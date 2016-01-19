c1825c535d9cbe092c882f676b0f7e8262d56956
Linghan Cheung <2661301@qq.com>
2016-01-19 09:57:24 +0800

    modify the ajax data

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -4,22 +4,29 @@ $(function ()
 {
     var ue = UE.getEditor('container', 
     {
-        serverUrl: "/admin/edit/control", 
+        serverUrl: "/admin/editor/control",    // request ueditor config.json
         initialFrameHeight: 300
     });
+    
     console.log(ue.getOpt("serverUrl")); 
-    ue.on("serverConfigLoaded", function(){console.log(ue.getOpt("imageActionName"));});
+    ue.on("serverConfigLoaded", function(){console.log("Ueditor config.json loaded");});
     
     $("#publishBtn").bind("click", function()
         {
-            console.log("click Publish button " + ue.getContent());
             $(".alert.alert-info").hide();
             $(".alert.alert-info").slideDown("slow");
-            $.ajax("/admin/edit/store", 
+            
+            var blog = {};
+            blog.title = $("#titleInput").val();
+            blog.content = ue.getContent();
+            console.log("click Publish button " + blog.title);   
+            
+            // post the blog
+            $.ajax("/admin/editor/store", 
             {
                 method: "post",
                 contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-                data: {content: ue.getContent()}
+                data: blog
             }).done(function(data)
             {
                 $(".alert.alert-info").hide();
@@ -34,5 +41,6 @@ $(function ()
             });
         }
     );
+    
 }
 );
