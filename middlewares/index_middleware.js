95d4ed939545cfbebc1705f5a326f1b1a9214187
Linghan Cheung <2661301@qq.com>
2016-03-10 16:27:41 +0800

    add collection tag

--- middlewares/index_middleware.js
+++ middlewares/index_middleware.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var dbtools = require("../database/dbtools");
+var events = require("events");
 var indexMiddleware = {};
 var blogsPerPage = 4;
 
@@ -8,34 +9,97 @@ indexMiddleware.getBlogListPageNum = function ()
 {
     function __getBlogListPageNum(req, res, next)
     {
-        dbtools.getAllBlogsCount(function(error, count)
+        // dbtools.getAllBlogsCount(function(error, count)
+        // {
+        //     if (error)
+        //     {
+        //         console.log(error);
+        //         res.status(500).end();
+        //     }
+        //     else
+        //     {
+        //         var pages = Math.floor(count / blogsPerPage) + 1;
+        //         var pageNum = Number(req.query.pageNum || "1");
+        //         dbtools.getBlogsPageNum(pageNum, blogsPerPage, function(error, result)
+        //         {
+        //             if (error)
+        //             {
+        //                 console.log(error);
+        //                 res.status(500).end();
+        //             }
+        //             else
+        //             {
+        //                 res.render("index", {pagesCnt: pages, blogs: result, curPageNum: pageNum});
+        //             }    
+        //         });
+        //     }
+        // });
+        
+        var collectionId = req.query.blogCollectionId;
+        if ( collectionId !== undefined && collectionId !== "")
         {
-            if (error)
+            
+        }
+        else
+        {
+            dbtools.getAllBlogsCount(function(error, count)
             {
-                console.log(error);
-                res.status(500).end();
-            }
-            else
-            {
-                var pages = Math.floor(count / blogsPerPage) + 1;
-                var pageNum = Number(req.query.pageNum || "1");
-                dbtools.getBlogsPageNum(pageNum, blogsPerPage, function(error, result)
+                if (error)
                 {
-                    if (error)
+                    console.log(error);
+                    res.status(500).end();
+                }
+                else
+                {
+                    var pages = Math.floor(count / blogsPerPage) + 1;
+                    var pageNum = Number(req.query.pageNum || "1");
+                    dbtools.getBlogsPageNum(pageNum, blogsPerPage, function(error, result)
                     {
-                        console.log(error);
-                        res.status(500).end();
-                    }
-                    else
-                    {
-                        res.render("index", {pagesCnt: pages, blogs: result, curPageNum: pageNum});
-                    }    
-                });
-            }
-        });
-        
-        // console.log(req.query.blogCollectionId);
-        
+                        if (error)
+                        {
+                            console.log(error);
+                            res.status(500).end();
+                        }
+                        else
+                        {
+                            var blogsWithCollections = [];
+                            var finishCnt = 0;
+                            
+                            var eventEmitter = new events.EventEmitter();
+                            // define a event emitter, then you can emit an event
+                            eventEmitter.on("render", function(error, result)
+                            {
+                                res.render("index", {pagesCnt: pages, blogs: blogsWithCollections, curPageNum: pageNum});
+                            });
+                            
+                            if (result.length === 0)
+                            {
+                                eventEmitter.emit("render");
+                            }
+                            else
+                            {
+                                for (let idx = 0; idx < result.length; idx++)
+                                {
+                                    var curCollectionId = result[idx].blogCollectionId || 0;
+                                    dbtools.getBlogCollectionById(curCollectionId, function(error, data)
+                                    {
+                                        var curBlog = {};
+                                        curBlog._id = result[idx]._id;
+                                        curBlog.title = result[idx].title;
+                                        curBlog.content = result[idx].content;
+                                        curBlog.lastEditDate = result[idx].lastEditDate;
+                                        if (data) curBlog.blogCollection = data.title;
+                                        blogsWithCollections.push(curBlog);
+                                        finishCnt++;
+                                        if (finishCnt >= result.length) eventEmitter.emit("render");
+                                    });
+                                }
+                            }
+                        }    
+                    });
+                }
+            });
+        }
     }
     
     return __getBlogListPageNum;
