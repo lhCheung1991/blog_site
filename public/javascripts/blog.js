701f62225b2f1451f240aebbf9b1781b380e29f8
Linghan Cheung <2661301@qq.com>
2016-03-17 21:51:15 +0800

    add user comment

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -10,4 +10,15 @@ $(function()
         $($header2[idx]).append('<a name=h2' + idx + '></a>');
         $categoryol.append('<li>' + '<a href="#h2' + idx + '">' + $($header2[idx]).text() + '</a>' + '</li>');
     }
+    
+    var ue = UE.getEditor('container', 
+    {
+        serverUrl: "/blogs/commenteditor/controller",    // request ueditor config.json
+        initialFrameHeight: 200,
+        elementPathEnabled : false,
+        toolbars: [["bold","italic","underline","fontborder","strikethrough",'superscript', 'subscript',
+        "forecolor","backcolor","justifyleft","justifycenter","justifyright","justifyjustify","fontfamily",
+        "fontsize","simpleupload","emotion","insertcode","removeformat","unlink","link","undo","redo"]]
+    });
+    
 });
--- routes/blogs_router.js
+++ routes/blogs_router.js
@@ -4,7 +4,16 @@ var express = require("express");
 var blogsRouter = express.Router();
 
 var blogsMiddleware = require("../middlewares/blogs_middleware");
+var ueditorController = require("../middlewares/ueditor_controller");
 
 blogsRouter.get("/checkblog", blogsMiddleware.checkBlog());    // /blogs/checkblog
+blogsRouter.all("/commenteditor/controller", ueditorController(
+    {
+        rootDir: "/Users/lhcheung1991/VScodeProjects/blog_site/",
+        imagesDir: "public/images/",
+        filesDir: "public/files/",
+        configJsonFile: "/Users/lhcheung1991/VScodeProjects/blog_site/middlewares/config.json"
+    })
+);
 
 module.exports = blogsRouter;
