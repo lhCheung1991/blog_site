e263518ea190bd42a1c93c3093b29688ab217f1f
Linghan Cheung <2661301@qq.com>
2016-03-19 10:30:31 +0800

    comment frontend

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -11,14 +11,86 @@ $(function()
         $categoryol.append('<li>' + '<a href="#h2' + idx + '">' + $($header2[idx]).text() + '</a>' + '</li>');
     }
     
-    var ue = UE.getEditor('container', 
+    var defaultEditor = UE.getEditor('container-default', 
     {
         serverUrl: "/blogs/commenteditor/controller",    // request ueditor config.json
-        initialFrameHeight: 200,
+        initialFrameHeight: 150,
         elementPathEnabled : false,
+        textarea: "commentContent",
         toolbars: [["bold","italic","underline","fontborder","strikethrough",'superscript', 'subscript',
         "forecolor","backcolor","justifyleft","justifycenter","justifyright","justifyjustify","fontfamily",
-        "fontsize","simpleupload","emotion","insertcode","removeformat","unlink","link","undo","redo"]]
+        "fontsize","simpleupload","emotion","insertcode","removeformat","unlink","link","undo","redo"]],
+        
     });
     
+    $("span.btn.btn-primary.btn-generate-editor").one("click", function()
+    {
+        // jQuery object is an array !!!
+        var blogId = $($(this).children()[0]).val();
+        var replyToId = $($(this).children()[1]).val();
+        var replyToName = $($(this).children()[2]).val();
+        
+        var $commentForm = generateCommentForm(blogId, replyToId, replyToName);
+        $commentForm.hide();
+        $(this).parent().after($commentForm);
+        
+        // initialize the editor
+        var editor = UE.getEditor('container-' + replyToId, 
+        {
+            serverUrl: "/blogs/commenteditor/controller",    // request ueditor config.json
+            initialFrameHeight: 150,
+            elementPathEnabled : false,
+            textarea: "commentContent",
+            toolbars: [["bold","italic","underline","fontborder","strikethrough",'superscript', 'subscript',
+            "forecolor","backcolor","justifyleft","justifycenter","justifyright","justifyjustify","fontfamily",
+            "fontsize","simpleupload","emotion","insertcode","removeformat","unlink","link","undo","redo"]],
+            
+        });
+        
+        $commentForm.slideDown("slow");
+        
+        $(this).text("收起");
+        
+        var state = true;
+        $(this).bind("click", function ()
+        {
+            if (state)
+            {
+                $commentForm.slideUp("slow");
+                $(this).text("回复");
+            }
+            else
+            {
+                $commentForm.slideDown("slow");
+                $(this).text("收起");  
+            }
+            
+            state = !state;
+        });
+        
+    });
+    
+    
 });
+
+
+function generateCommentForm(blogId, replyToId, replyToName)
+{
+    var $commentForm = $('<form class="form-horizontal" action="/blogs/commentcommit/" method="post"></form>');
+    $commentForm.append('<input type="hidden" value="' + blogId + '" name="blogId"/>');
+    $commentForm.append('<input type="hidden" value="' + replyToId + '" name="replyToId"/>');
+    $commentForm.append('<input type="hidden" value="' + replyToName + '" name="replyToName"/>');
+    
+    $commentForm.append(
+        '<div class="form-group"><label for="nickNameId" class="col-sm-2 control-label">Nickname</label><div class="col-sm-10"><input type="text" class="form-control" id="nickNameId" placeholder="Nickname for displaying" name="nickName"/></div></div>'
+        );
+    $commentForm.append(
+        '<div class="form-group"><label for="emailId" class="col-sm-2 control-label">E-mail</label><div class="col-sm-10"><input type="text" class="form-control" id="emailId" placeholder="E-mail for contacting" name="email"/></div></div>'
+        );
+
+    $commentForm.append('<div class="form-group"><script id="container-' + replyToId + '" name="commentContent" type="text/plain"></script></div>');
+    
+    $commentForm.append('<button class="btn btn-success" type="submit">提交</button>');
+
+    return $commentForm;
+}
