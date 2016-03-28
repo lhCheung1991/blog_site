88db3d35065aa878d9e35696f3a32719692ac5c4
Linghan Cheung <2661301@qq.com>
2016-03-28 15:06:42 +0800

    add syntaxhighlighter

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -11,6 +11,43 @@ $(function()
         $categoryol.append('<li>' + '<a href="#h2' + idx + '">' + $($header2[idx]).text() + '</a>' + '</li>');
     }
     
+    function path() {
+        var args = arguments,
+        result = [];
+        for(var i = 0; i < args.length; i++)
+            result.push(args[i].replace('@', 'ueditor/third-party/SyntaxHighlighter/syntaxhighlighter/scripts/'));
+        return result;
+    };
+    SyntaxHighlighter.autoloader.apply(null, path(
+    'applescript            @shBrushAppleScript.js',
+    'actionscript3 as3      @shBrushAS3.js', 
+    'bash shell             @shBrushBash.js', 
+    'coldfusion cf          @shBrushColdFusion.js',
+    'cpp c                  @shBrushCpp.js',
+    'c# c-sharp csharp      @shBrushCSharp.js', 
+    'css                    @shBrushCss.js', 
+    'delphi pascal          @shBrushDelphi.js', 
+    'diff patch pas         @shBrushDiff.js', 
+    'erl erlang             @shBrushErlang.js',
+    'groovy                 @shBrushGroovy.js', 
+    'java                   @shBrushJava.js',
+    'jfx javafx             @shBrushJavaFX.js', 
+    'js jscript javascript  @shBrushJScript.js',
+    'perl pl                @shBrushPerl.js',
+    'php                    @shBrushPhp.js', 
+    'text plain             @shBrushPlain.js', 
+    'py python              @shBrushPython.js',
+    'ruby rails ror rb      @shBrushRuby.js',
+    'sass scss              @shBrushSass.js',
+    'scala                  @shBrushScala.js',
+    'sql                    @shBrushSql.js', 
+    'vb vbnet               @shBrushVb.js',
+    'xml xhtml xslt html    @shBrushXml.js'
+    ));
+    SyntaxHighlighter.defaults['smart-tabs'] = true;
+    SyntaxHighlighter.defaults['tab-size'] = 4;
+    SyntaxHighlighter.all();
+    
     var defaultEditor = UE.getEditor('container-default', 
     {
         serverUrl: "/blogs/commenteditor/controller",    // request ueditor config.json
@@ -98,7 +135,42 @@ function generateCommentForm(blogId, replyToId, replyToName)
     return $commentForm;
 }
 
-function checkCommentForm()
-{
-    
-}
+// $(document).ready(function ()
+// {
+//     function path() {
+//         var args = arguments,
+//         result = [];
+//         for(var i = 0; i < args.length; i++)
+//             result.push(args[i].replace('@', 'ueditor/third-party/SyntaxHighlighter/syntaxhighlighter/scripts/'));
+//         return result;
+//     };
+//     SyntaxHighlighter.autoloader.apply(null, path(
+//     'applescript            @shBrushAppleScript.js',
+//     'actionscript3 as3      @shBrushAS3.js', 
+//     'bash shell             @shBrushBash.js', 
+//     'coldfusion cf          @shBrushColdFusion.js',
+//     'cpp c                  @shBrushCpp.js',
+//     'c# c-sharp csharp      @shBrushCSharp.js', 
+//     'css                    @shBrushCss.js', 
+//     'delphi pascal          @shBrushDelphi.js', 
+//     'diff patch pas         @shBrushDiff.js', 
+//     'erl erlang             @shBrushErlang.js',
+//     'groovy                 @shBrushGroovy.js', 
+//     'java                   @shBrushJava.js',
+//     'jfx javafx             @shBrushJavaFX.js', 
+//     'js jscript javascript  @shBrushJScript.js',
+//     'perl pl                @shBrushPerl.js',
+//     'php                    @shBrushPhp.js', 
+//     'text plain             @shBrushPlain.js', 
+//     'py python              @shBrushPython.js',
+//     'ruby rails ror rb      @shBrushRuby.js',
+//     'sass scss              @shBrushSass.js',
+//     'scala                  @shBrushScala.js',
+//     'sql                    @shBrushSql.js', 
+//     'vb vbnet               @shBrushVb.js',
+//     'xml xhtml xslt html    @shBrushXml.js'
+//     ));
+//     SyntaxHighlighter.defaults['smart-tabs'] = true;
+//     SyntaxHighlighter.defaults['tab-size'] = 4;
+//     SyntaxHighlighter.all();
+// });
